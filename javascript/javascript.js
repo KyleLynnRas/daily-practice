7db510b92501ec6fe1aa62b04a6af75e6b4ae86f
KyleLynnRas <kylelynn.ras@gmail.com>
2021-08-08 06:19:47 -0400

    Add challenge

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -55,13 +55,13 @@ function factorial(n) {
 //https://www.codewars.com/kata/609eee71109f860006c377d1/train/javascript
 
 function lastSurvivor(letters, coords) {
-  const letterArr = letters.split("")
-  let coordLength = letterArr.length -1
-  for (let i = 0; i < coordLength; i++) {
-    letterArr.splice(coords[i], 1)
-  }
-  const answer = letterArr.join()
-  return answer
+	const letterArr = letters.split("");
+	let coordLength = letterArr.length - 1;
+	for (let i = 0; i < coordLength; i++) {
+		letterArr.splice(coords[i], 1);
+	}
+	const answer = letterArr.join();
+	return answer;
 }
 
 // lastSurvivor("abc", [1,1])
@@ -69,37 +69,58 @@ function lastSurvivor(letters, coords) {
 //https://www.codewars.com/kata/59377c53e66267c8f6000027/train/javascript
 
 function alphabetWar(fight) {
-  let rightSd = 0
-  let leftSd = 0
+	let rightSd = 0;
+	let leftSd = 0;
 
-  const stringArr = fight.split("")
+	const stringArr = fight.split("");
 
-  stringArr.forEach((ele) => {
-    if (ele === "w") {
-      leftSd = leftSd + 4
-    } else if (ele === "p") {
-      leftSd = leftSd + 3
-    } else if (ele === "b") {
-      leftSd = leftSd + 2
-    } else if (ele === "s") {
-      leftSd = leftSd + 1
-    } else if (ele === "m") {
-      rightSd = rightSd + 4
-    } else if (ele === "q") {
-      rightSd = rightSd + 3
-    } else if (ele === "d") {
-      rightSd = rightSd + 2
-    } else if (ele === "z") {
-      rightSd = rightSd + 1
-    }
-  })
+	stringArr.forEach((ele) => {
+		if (ele === "w") {
+			leftSd = leftSd + 4;
+		} else if (ele === "p") {
+			leftSd = leftSd + 3;
+		} else if (ele === "b") {
+			leftSd = leftSd + 2;
+		} else if (ele === "s") {
+			leftSd = leftSd + 1;
+		} else if (ele === "m") {
+			rightSd = rightSd + 4;
+		} else if (ele === "q") {
+			rightSd = rightSd + 3;
+		} else if (ele === "d") {
+			rightSd = rightSd + 2;
+		} else if (ele === "z") {
+			rightSd = rightSd + 1;
+		}
+	});
 
-  let answer 
+	let answer;
 
-  rightSd > leftSd ? answer = "Right side wins!" : leftSd > rightSd ? answer = "Left side wins!" : answer = "Let's fight again!"
+	rightSd > leftSd
+		? (answer = "Right side wins!")
+		: leftSd > rightSd
+		? (answer = "Left side wins!")
+		: (answer = "Let's fight again!");
 
-  return answer 
+	return answer;
 }
 
-
 // console.log(alphabetWar("meow"))
+
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+function noSpace(x) {
+	let newString = x.split("");
+	let stringArr = newString.filter((ele) => {
+		if (ele !== " ") {
+			return ele;
+		}
+	});
+	const answer = stringArr.join("");
+	return answer;
+}
+
+noSpace("testing 123");
+
+//https://www.codewars.com/kata/57f759bb664021a30300007d
+
