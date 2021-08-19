a026230ca6ea43d8c24190a5343d0b2590ba06c1
KyleLynnRas <kylelynn.ras@gmail.com>
2021-08-18 22:50:09 -0400

    Add challenges

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -125,11 +125,60 @@ noSpace("testing 123");
 //https://www.codewars.com/kata/5aa736a455f906981800360d/train/javascript
 
 function feast(beast, dish) {
-	const beastArr = beast.split("")
-	const dishArr = dish.split("")
-	let answer 
-	dishArr[0] === beastArr[0] && dishArr[dishArr.length - 1] === beastArr[beastArr.length - 1] ? answer = true : answer = false
-	return answer 
-  }
+	const beastArr = beast.split("");
+	const dishArr = dish.split("");
+	let answer;
+	dishArr[0] === beastArr[0] &&
+	dishArr[dishArr.length - 1] === beastArr[beastArr.length - 1]
+		? (answer = true)
+		: (answer = false);
+	return answer;
+}
 
-feast("chickadee", "chocolate cake")
+feast("chickadee", "chocolate cake");
+
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
+
+class SmallestIntegerFinder {
+	findSmallestInt(args) {
+		let answer = Math.min(...args);
+		return answer;
+	}
+}
+
+//https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+
+var isSquare = function (n) {
+	if (n >= 0) {
+		let square = Math.sqrt(n);
+		square = Math.round(square);
+		console.log(square);
+		let answer;
+		const squareRt = square * square;
+		squareRt === n ? (answer = true) : (answer = false);
+		return answer;
+	} else {
+		return false;
+	}
+};
+
+isSquare(25);
+
+//https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/javascript
+
+function vaporcode(string) {
+	//to uppercase
+	let vaporString = string.toUpperCase();
+	vaporString = vaporString.split("");
+
+	vaporString.map((ele, index) => {
+		if (ele === " ") {
+			vaporString.splice(index, 1);
+		}
+	});
+
+	vaporString = vaporString.join("  ");
+	return vaporString;
+}
+
+vaporcode("meow's today");
