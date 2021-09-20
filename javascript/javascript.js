d7c936e695531221171bd29e2fad6c0cb4634bca
KyleLynnRas <kylelynn.ras@gmail.com>
2021-09-20 17:43:34 -0400

    Add challenge

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -910,3 +910,20 @@ const miniMaxSum = (arr) => {
 };
 
 // miniMaxSum([1,2, 3, 4, 5])
+
+//https://leetcode.com/problems/remove-element/submissions/
+
+const removeElement = (nums, val) => {
+	
+	for (let i = 0; i < nums.length; i++) {
+		
+		if (nums[i] === val) {
+			nums.splice(i, 1)
+			i--
+		}
+	}
+
+	return nums.length
+}
+
+console.log((removeElement([3,2,2,3], 3)))
